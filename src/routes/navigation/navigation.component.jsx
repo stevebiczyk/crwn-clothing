b62143925ec4d4f45bde174a882b7ae68c303e27
stevebiczyk <stevebiczyk@gmail.com>
2024-02-09 21:59:56 +0000

    added crown logo and fixed errors caused by using Vite instead of CRA

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import CrownLogo from "../../assets/CrownLogo";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          {/* <div className="logo-div">Logo</div> */}
+          <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
