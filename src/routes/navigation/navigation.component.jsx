c709bdc75a62f37ec3d4e8ec243b8597401e337f
stevebiczyk <stevebiczyk@gmail.com>
2024-03-02 17:03:55 +0000

    added signing out function for current user

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,25 +2,36 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import CrownLogo from "../../assets/CrownLogo";
 import { UserContext } from "../../contexts/user.context";
+import { signOutCurrentUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    await signOutCurrentUser();
+    setCurrentUser(null);
+  };
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          {/* <div className="logo-div">Logo</div> */}
           <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
-          <Link className="nav-link" to="/auth">
-            Sign In
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutCurrentUser}>
+              Sign Out
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              Sign In
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
