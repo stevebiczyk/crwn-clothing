5e2e860949c2f024ae8d3c000e6c16f43e0ef066
stevebiczyk <stevebiczyk@gmail.com>
2024-03-02 10:49:45 +0000

    bringing local folder up to date

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    try {
+      // Attempt to create a new user with email and password
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      // Create a user document in Firestore
+      await createUserDocumentFromAuth(user, { displayName });
+      alert("User signed up successfully!");
+
+      // Reset the form fields
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use.");
+      } else {
+        console.log("Error creating user", error.message);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>Sign In with your email and password</h1>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          inputOptions={{
+            type: "text",
+            onChange: handleChange,
+            name: "displayName",
+            value: displayName,
+            required: true,
+          }}
+        />
+        <FormInput
+          label="Email"
+          inputOptions={{
+            type: "email",
+            onChange: handleChange,
+            name: "email",
+            value: email,
+            required: true,
+          }}
+          // type="email"
+          // onChange={handleChange}
+          // name="email"
+          // value={email}
+          // required
+        />
+        <FormInput
+          label="Password"
+          inputOptions={{
+            type: "password",
+            onChange: handleChange,
+            name: "password",
+            value: password,
+            required: true,
+          }}
+          // type="password"
+          // onChange={handleChange}
+          // name="password"
+          // value={password}
+          // required
+        />
+        <FormInput
+          label="Confirm Password"
+          inputOptions={{
+            type: "password",
+            onChange: handleChange,
+            name: "confirmPassword",
+            value: confirmPassword,
+            required: true,
+          }}
+          // type="password"
+          // onChange={handleChange}
+          // name="confirmPassword"
+          // value={confirmPassword}
+          // required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -5,7 +5,7 @@ import {
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
   const handleSignInWithGoogle = async () => {
     const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
@@ -22,4 +22,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
