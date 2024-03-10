fedf6d616cf326a823483c04fddaa9e57364a631
stevebiczyk <stevebiczyk@gmail.com>
2024-03-10 09:52:50 +0000

    created checkout page

---
+++ src/components/checkout/checkout.component.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
+import "./checkout.styles.scss";
+
+const Checkout = () => {
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  return (
+    <div className="checkout-container">
+      <h1>Checkout Page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity, price } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>
+                {quantity} x ${price}
+              </span>
+              <br />
+              <span>Decrease</span>
+              <br />
+              <span onClick={() => addItemToCart(cartItem)}>Increase</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
