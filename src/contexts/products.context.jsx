e9bdfd1e4cac1bc07576d1a329f017803ec456ee
stevebiczyk <stevebiczyk@gmail.com>
2024-03-13 21:47:29 +0000

    import products and categories from firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,17 +1,20 @@
 import { createContext, useEffect, useState } from "react";
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
-// import SHOP_DATA from "../shop-data.js"; // Importing the shop data
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
-  setProducts: () => null,
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   addCollectionAndDocuments("categories", SHOP_DATA);
-  // }, []);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
 
   return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -16,6 +16,8 @@ import {
   setDoc,
   collection,
   writeBatch,
+  query,
+  getDocs,
 } from "firebase/firestore";
 
 // const firebaseConfig = {
@@ -66,6 +68,19 @@ export const addCollectionAndDocuments = async (
 
   await batch.commit();
   console.log("collections added");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+  return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
