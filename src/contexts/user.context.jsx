db0ef6b3cdfc7ec91861aa8f89b14e2509d59116
stevebiczyk <stevebiczyk@gmail.com>
2024-03-21 08:05:40 +0000

    added useReducer to user.context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
@@ -10,8 +10,36 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+  SIGN_OUT: "SIGN_OUT",
+};
+
+const userReducer = (state, action) => {
+  console.log("dispatched action:");
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    // case "SIGN_OUT":
+    //   return { currentUser: null };
+    default:
+      throw new Error(`Unsupported action type: ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = { currentUser: null };
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
@@ -27,3 +55,10 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+const userReducer = (state, action) => {
+  return { currentUser: null };
+};
+
+*/
