55962ead095242bb0e0224567a33221bf525c30e
stevebiczyk <stevebiczyk@gmail.com>
2024-05-05 11:03:11 +0100

    set up Sripe payment form

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,28 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
+import { FormContainer, PaymentFormContainer } from "./payment-form.styles";
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const PaymentHandler = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Enter your card details: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
