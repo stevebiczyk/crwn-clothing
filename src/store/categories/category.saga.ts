548225b736016308d369b1a4ec9b3b9fe3cc0531
stevebiczyk <stevebiczyk@gmail.com>
2024-05-28 23:37:16 +0100

    typed the category saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import {
   fetchCategoriesSuccess,
@@ -8,10 +8,13 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(
+      getCategoriesAndDocuments,
+      "categories"
+    );
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailure(error));
+    yield put(fetchCategoriesFailure(error as Error));
   }
 }
 
