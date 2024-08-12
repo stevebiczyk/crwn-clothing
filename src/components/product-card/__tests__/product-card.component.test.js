2078c59712c9e00c2eb0985df77230cbf3de964f
stevebiczyk <stevebiczyk@gmail.com>
2024-08-12 21:09:48 +0100

    added Product Card tests

---
+++ src/components/product-card/__tests__/product-card.component.test.js
@@ -0,0 +1,26 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithProviders } from "../../../utils/test-utils";
+import ProductCard from "../product-card.component";
+
+describe("ProductCard tests", () => {
+  test("it should add the product when Product card button is clicked", () => {
+    const mockProduct = {
+      id: 1,
+      imageUrl: "test",
+      name: "Item A",
+      price: 100,
+    };
+    const { store } = renderWithProviders(<ProductCard product={mockProduct} />, {
+      preloadedState: {
+        cart: {
+          cartItems: [],
+        },
+      },
+    });
+  });
+  const addTocartButtonElement = screen.getByText(/add to cart/i)
+  await fireEvent.click (addTocartButtonElement.click);
+
+  expect(store.getState().cart.cartItems.lenght).toEqual(1);
+});
