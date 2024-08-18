2843bc15c4f4964ffe684e0428607dbc88d39692
stevebiczyk <stevebiczyk@gmail.com>
2024-08-18 12:44:58 +0100

    category tests and react-router-dom mocks

---
+++ src/routes/category/__tests__/category.component.tests.js
@@ -0,0 +1,67 @@
+import { screen } from "@testing-library/react";
+
+import Category from "../category.component";
+import { renderWithProviders } from "../../../utils/test-utils";
+import { title } from "process";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    category: "mens",
+  }),
+}));
+
+describe("Category tests", () => {
+  test("It should render a spinner if isLoading is true", () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: true,
+          categories: [],
+        },
+      },
+    });
+
+    const spinnerElement = screen.getByTestId("spinner");
+    expect(spinnerElement).toBeInTheDocument();
+  });
+
+  test("It should render no spinner if isLoading is false", () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: true,
+          categories: [],
+        },
+      },
+    });
+
+    const spinnerElement = screen.getByTestId("spinner");
+    expect(spinnerElement).toBeNull();
+  });
+
+  test("It should render products if isLoading is false and there are items present", () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: true,
+          categories: [
+            {
+              title: "mens",
+              items: [
+                { id: 1, name: "Product 1" },
+                { id: 2, name: "Product 2" },
+              ],
+            },
+          ],
+        },
+      },
+    });
+
+    const spinnerElement = screen.getByTestId("spinner");
+    expect(spinnerElement).toBeNull();
+
+    const product1Element = screen.getByText(/product 1/i);
+    expect(product1Element).toBeInTheDocument();
+  });
+});
