af39d21165a6a734f5804ad473368a0aaffd6a37
stevebiczyk <stevebiczyk@gmail.com>
2024-08-24 15:19:52 +0100

    category saga tests - part 1

---
+++ src/routes/navigation/__tests__/category.saga.tests.js
@@ -0,0 +1,31 @@
+import { call } from "typed-redux-saga/macro";
+import { testSaga } from "redux-saga-test-plan";
+
+import {
+  fetchCategoriesAsync,
+  onFetchCategories,
+  categoriesSaga,
+} from "../category.saga";
+
+import { CATEGORIES_ACTION_TYPES } from "../category.types";
+
+describe("category sagas", () => {
+  test("categoriesSaga", () => {
+    testSaga(categoriesSaga)
+      .next()
+      .all([call(onFetchCategories)])
+      .next()
+      .isDone();
+  });
+
+  test("onFetchCategories", () => {
+    testSaga(onFetchCategories)
+      .next()
+      .takeLatest(
+        "CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START",
+        fetchCategoriesAsync
+      )
+      .next()
+      .isDone();
+  });
+});
